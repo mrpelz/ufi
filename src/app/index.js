9de75bee69756e08a4a9ea1b532d1d0099aab329
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2019-12-04 12:33:38 +0100

    create test case for video layer

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 const { Asset } = require('../lib/asset');
 const { Display } = require('../lib/display');
 const { DisplayServer } = require('../lib/server');
-const { ModuleLayer } = require('../lib/layer');
+const { ModuleLayer, VideoLayer } = require('../lib/layer');
 const { Presentation } = require('../lib/presentation');
 
 const server = new DisplayServer({ port: 1337 });
@@ -24,9 +24,27 @@ const clockLayer = new ModuleLayer(
   })
 );
 
+const videoLayer = new VideoLayer(
+  new Asset('http://ufi.mom.net.wurstsalat.cloud/ufi-assets/bin/greenland_3.mp4', {
+    type: 'video'
+  })
+);
+
 const presentation0 = new Presentation();
 
-presentation0.setLayers([clockLayer]);
+presentation0.preloadLayer(clockLayer);
+presentation0.preloadLayer(videoLayer);
+
+const states = [
+  [videoLayer, clockLayer],
+  [videoLayer]
+];
+let counter = 0;
+
+setInterval(() => {
+  presentation0.setLayers(states[counter]);
+  counter = counter === (states.length - 1) ? 0 : counter + 1;
+}, 10000);
 
 displayTest.setPresentations([
   presentation0
@@ -34,5 +52,3 @@ displayTest.setPresentations([
 displayArbeitszimmer.setPresentations([
   presentation0
 ]);
-
-module.exports = undefined;
