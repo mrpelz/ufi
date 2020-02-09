f151b5f34492aba95a6ae2dad374b114ffb137d1
Lennart Pelz <lennart.pelz@sinnerschrader.com>
2020-02-09 15:51:53 +0100

    remove video from demo presentation

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 const { Asset } = require('../lib/asset');
 const { Display } = require('../lib/display');
 const { DisplayServer } = require('../lib/server');
-const { ModuleLayer, VideoLayer } = require('../lib/layer');
+const { ModuleLayer } = require('../lib/layer');
 const { Presentation } = require('../lib/presentation');
 
 const server = new DisplayServer({ port: 1337 });
@@ -28,31 +28,18 @@ clockLayer.setState({
   data: {
     borderColor: null,
     centerColor: null,
-    faceColor: '#FFFFFF80',
+    faceColor: '#FFFFFF',
     fillColor: null,
-    labelColor: '#FFFFFF80',
-    hoursHandColor: '#FFFFFF80',
-    minutesHandColor: '#FFFFFF80',
-    secondsHandColor: '#FF000080'
-  }
-});
-
-const videoLayer = new VideoLayer(
-  new Asset('http://ufi.mom.net.wurstsalat.cloud/ufi-assets/bin/greenland_3.mp4', {
-    type: 'video'
-  })
-);
-
-videoLayer.setState({
-  layout: {
-    spanColumns: 12,
-    spanRows: 12
+    labelColor: '#FFFFFF',
+    hoursHandColor: '#FFFFFF',
+    minutesHandColor: '#FFFFFF',
+    secondsHandColor: '#FF0000'
   }
 });
 
 const presentation0 = new Presentation();
 
-presentation0.setLayers([videoLayer, clockLayer]);
+presentation0.setLayers([clockLayer]);
 
 displayTest.setPresentations([
   presentation0
